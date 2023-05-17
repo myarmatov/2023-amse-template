5fd856419595cd37a06bcccabad693d64a516b0b
myarmatov <74456299+myarmatov@users.noreply.github.com>
2023-05-17 16:33:07 +0200

    Update etl.py

--- data/etl.py
+++ data/etl.py
@@ -3,6 +3,12 @@ import io
 import requests
 from sqlalchemy import create_engine
 
+url1 = "https://offenedaten-konstanz.de/sites/default/files/Zaehlstelle_Herose_2019_stuendlich_Wetter.csv"
+encoding1 = "ISO-8859-1"
+url2 = "https://bicycle.vlba.uni-oldenburg.de/csv?region=Konstanz&table=zaehl&startDate=2019-01-01&endDate=2019-12-31&stations=Herosépark&columns=countTo&columns=countFrom&columns=classification"
+encoding2 = "utf-8"
+
+
 def save_dfToSql(df, name):
 
     # Create a connection to the SQLite database using SQLAlchemy
@@ -16,26 +22,17 @@ def save_dfToSql(df, name):
     engine.dispose()
 
 
+    
 ###Extract
 
 #Datasource 1
-url1 = "https://offenedaten-konstanz.de/sites/default/files/Zaehlstelle_Herose_2019_stuendlich_Wetter.csv"
-encoding1 = "ISO-8859-1"
 df1 = pd.read_csv(url1, encoding = encoding1, sep=';')
 
-
 #Datasource 2
-
-url2 = "https://bicycle.vlba.uni-oldenburg.de/csv?region=Konstanz&table=zaehl&startDate=2019-01-01&endDate=2019-12-31&stations=Herosépark&columns=countTo&columns=countFrom&columns=classification"
-encoding2 = "utf-8"
-
 response = requests.get(url2)
 response.raise_for_status()
-
-# Create a pandas DataFrame from the downloaded content
 content = response.content.decode(encoding2)
 df2 = pd.read_csv(io.StringIO(content))
-
 
 
 
@@ -47,6 +44,3 @@ df2 = df2.drop('station', axis=1)
 ###Load
 save_dfToSql(df1, "bikeCountKonstanz_ds1")
 save_dfToSql(df2, "bikeCountKonstanz_ds2")
-
-
-
